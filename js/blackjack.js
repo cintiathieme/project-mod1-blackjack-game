4fa69ecefa311f70a4fd332b359d17aa5af3b654
cintiathieme <cintiathieme@gmail.com>
2021-02-25 23:28:14 -0300

    done

--- js/blackjack.js
+++ js/blackjack.js
@@ -20,14 +20,44 @@ class Game {
             this.cards.push(selectedCard);
            
             
-            this.pickedCardsDealer = [this.cards[0], this.cards[2]],
-            this.pickedCardsPlayer = [this.cards[5], this.cards[8]],
-            this.thirdCardDealer = [this.cards[1]],
-            this.thirdCardPlayer = [this.cards[6]]
-        }; 
+            this.pickedCardsDealer = [this.cards[2], this.cards[3]],
+            this.pickedCardsPlayer = [this.cards[0], this.cards[1]],
 
-        return {cardsDealer:this.pickedCardsDealer, cardsPlayer: this.pickedCardsPlayer, card3Dealer: this.thirdCardDealer, card3Player: this.thirdCardPlayer};     
+            
+        document.querySelector('#dealerHideCard').innerHTML =`<div class="card" data-card-name="${this.pickedCardsDealer[1].name}">
+        <img id="hideCard" class="show" src="./images/baralho.png">
+        </div>`    
+
+        document.querySelector('#dealer').innerHTML = `<div class="card" data-card-name="${this.pickedCardsDealer[0].name}">
+        <img class="show" src="${this.pickedCardsDealer[0].img}">
+        </div>
+        </div>`;
+
+           
+        document.querySelector('#player').innerHTML = `<div class="card" data-card-name="${this.pickedCardsPlayer[0].name}">
+        <img class="show" src="${this.pickedCardsPlayer[0].img}">
+        </div>
+        </div>
+        <div class="card" data-card-name="${this.pickedCardsPlayer[1].name}">
+        <img class="show" src="${this.pickedCardsPlayer[1].img}">
+        </div>
+        </div>`;
+
+        }
+        this.cards.splice(0,4)
+            
+        
+        return {cardsDealer: this.pickedCardsDealer, cardsPlayer: this.pickedCardsPlayer};     
     };
+
+    sumCardsPlayer(array) {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i]
+        }
+        document.querySelector('#playerSum').innerHTML = `<div class="points"> ${sum}</div>`
+        return sum;
+    }
 
     checkIf21(array) {
         let sum = 0;
@@ -35,39 +65,77 @@ class Game {
             sum += array[i]
         }
         if (sum === 21) {
+            let hitDisable = `<div class="buttonDisable">Hit</div>`;
+        document.querySelector('#hit').innerHTML = hitDisable;
+
+        let standDisable = `<div class="buttonDisable">Stand</div>`;
+        document.querySelector('#stand').innerHTML = standDisable;
             return true;
         }
         return false;
     }
 
-    sumCards(array) {
+    winner() {
+        setTimeout(() => {
+            document.querySelector('section').innerHTML = '<img class="winner" src="./images/winner.png">';
+        },3000)
+    }
+    
+    loser() {
+        setTimeout(() => {        
+            document.querySelector('section').innerHTML = '<img class="loser" src="./images/loser.png">';
+        },3000)
+    }
+    
+    addCardPlayer(){
+        let addPlayer = this.cards[0];
+
+        this.pickedCardsPlayer.unshift(addPlayer);
+        
+
+        let newCardPlayer = `<div class="card" data-card-name="${this.pickedCardsPlayer[0].name}">
+        <img class="show" src="${this.pickedCardsPlayer[0].img}">
+        </div>
+        </div>`
+        document.querySelector('#player').insertAdjacentHTML(`afterend`, newCardPlayer);
+
+        this.cards.splice(0,1);
+
+        return addPlayer;
+    }
+
+    addCardDealer(number){
+        
+            let addDealer = this.cards[0];
+    
+            this.pickedCardsDealer.unshift(addDealer);
+    
+            let newCardDealer = `<div class="card" data-card-name="${this.pickedCardsDealer[0].name}">
+            <img class="show" src="${this.pickedCardsDealer[0].img}">
+               </div>
+               </div>`
+                 
+            this.cards.splice(0,1)
+
+            document.querySelector('#dealer').insertAdjacentHTML(`afterend`, newCardDealer);
+
+            return addDealer;
+    }
+
+    sumCardsDealer(array) {
         let sum = 0;
         for (let i = 0; i < array.length; i++) {
             sum += array[i]
         }
+        
         return sum;
-    }
-
-    
-    compareCards(sum1, sum2) {
-      
-        if (sum1 > sum2 && sum1 <= 21) {
-            
-            return true;
-         
-        }
-        return false;
-
     }
 
     endGame (){
         setTimeout(() => {
             document.location.reload()
-        }, 8000);
+        }, 5000);
     }
     
-   removeButton (){
-       let button = document.querySelectorAll('.btn');
-       button.removeEventListener
-   }
+   
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-// 1- iniciar o jogo e embaralhar as cartas,
-// 2- 2 cartas para o primeiro jogador e 2 cartas para o segundo jogador,
-// 3- virar as cartas do segundo jogador,
-// 4- somar os valores das cartas - se algum for igual a 21 o jogo acaba,
-// 5- se não acabar, o segundo jogador escolhe se quer mais uma carta,
-// 6- se a soma for maior do que 21 o jogo acaba,
-// 5- somar o valor das cartas do primeiro jogador e, se for menor/igual a 11, pegar mais uma carta,
-// 6 - comparar o valor das cartas e quem tiver a maior soma ganha
-
-
 const cards = [
     {name: '2espada', value: 2, img: './images/2-de-espada.png'},
     {name: '3espada',value: 3, img: './images/3-de-espada.png'},
@@ -28,72 +18,74 @@ const firstPage = document.querySelector('#firstPage');
 const gameBoard = document.querySelector('#gameBoard');
 
 document.querySelector('.play').addEventListener('click', () => {
-
+    
     firstPage.className = "hide";
     gameBoard.className = "show";
 })
 
 let start = document.querySelector('.start')
 
+const game = new Game(cards);
+
 start.addEventListener('click', function click () {
-    start.removeEventListener('click',click);
+    start.removeEventListener('click', click);
     
-    const game = new Game(cards);
-
-  
     const shuffleCards = game.shuffleCards();
-
-    console.log(shuffleCards)
-
-    
-    let dealer = `<div class="card" data-card-name="${shuffleCards.cardsDealer[0].name}">
-        <img id="hideCard" class="show" src="./images/baralho.png">
-        </div>
-        <div class="card" data-card-name="${shuffleCards.cardsDealer[1].name}">
-        <img class="show" src="${shuffleCards.cardsDealer[1].img}">
-        </div>
-        </div>`;
-
-        document.querySelector('#dealer').innerHTML = dealer;
-
-    
-    let player = `<div class="card" data-card-name="${shuffleCards.cardsPlayer[0].name}">
-    <img class="show" src="${shuffleCards.cardsPlayer[0].img}">
-    </div>
-    </div>
-    <div class="card" data-card-name="${shuffleCards.cardsPlayer[1].name}">
-    <img class="show" src="${shuffleCards.cardsPlayer[1].img}">
-    </div>
-    </div>`;
-
-    document.querySelector('#player').innerHTML = player;
+       
 
     let playersHand = [shuffleCards.cardsPlayer[0].value, shuffleCards.cardsPlayer[1].value]
-    
+
     let dealersHand = [shuffleCards.cardsDealer[0].value, shuffleCards.cardsDealer[1].value]
 
+    let sumCardsPlayer = game.sumCardsPlayer(playersHand);
+    let sumCardsDealer = game.sumCardsDealer(dealersHand);
+
     if (game.checkIf21(playersHand)) {
-        let win = '<img src="./images/winner.png">'
-    
-            setTimeout(() => {
-    
-                document.querySelector('section').innerHTML = win;
-            },1000)
+        game.winner();
+        game.endGame();
+    }
 
+    document.querySelector('.hit').addEventListener('click', () => {
+       let addCardPlayer = game.addCardPlayer();
+    
+        playersHand.push(addCardPlayer.value)
+
+        sumCardsPlayer = game.sumCardsPlayer(playersHand)
+
+        if (game.checkIf21(playersHand)) {
+            game.winner();
             game.endGame();
-    }
+        }
 
-    if (game.checkIf21(dealersHand)) {
+        if (game.sumCardsPlayer(playersHand) > 21) {
+            let hitDisable = `<div class="buttonDisable">Hit</div>`;
+        document.querySelector('#hit').innerHTML = hitDisable;
+            game.loser();
+            game.endGame();
+        } else {
+            if (sumCardsDealer < 17) {
+                let addCardDealer = game.addCardDealer(sumCardsDealer);
+                dealersHand.push(addCardDealer.value)
+            
+                sumCardsDealer = game.sumCardsDealer(dealersHand);
+                
+                if (sumCardsDealer > 21) {
+                    let hitDisable = `<div class="buttonDisable">Hit</div>`;
+                    document.querySelector('#hit').innerHTML = hitDisable;
+                    
+                    document.querySelector('#dealerSum').innerHTML = `<div class="points"> ${sumCardsDealer}</div>`
+                    
+                    let card = shuffleCards.cardsDealer.length -1
+                    
+                    let dealer = `<img id="hideCard" class="show" src="${shuffleCards.cardsDealer[card].img}">`
+                    
+                    game.winner();
+                    game.endGame();
+                }
+            }   
+        }
+    });
 
-        let lose = '<img src="./images/loser.png">'
-        
-                setTimeout(() => {
-        
-                    document.querySelector('section').innerHTML = lose;
-                },1000)
-    
-                game.endGame();
-    }
 
     document.querySelector('.stand').addEventListener('click', () => {
         let hitDisable = `<div class="buttonDisable">Hit</div>`;
@@ -102,166 +94,52 @@ start.addEventListener('click', function click () {
         let standDisable = `<div class="buttonDisable">Stand</div>`;
         document.querySelector('#stand').innerHTML = standDisable;
 
-    let sumPlayer = game.sumCards(playersHand);
-    let addPlayersPoints = `<div class="points"> ${sumPlayer}</div>`
-        document.querySelector('#playerSum').innerHTML = addPlayersPoints;
-     
-    let sumDealer = game.sumCards(dealersHand)
+        if (sumCardsDealer < 17) {let addCardDealer = game.addCardDealer(sumCardsDealer);
+            dealersHand.push(addCardDealer.value)
+            sumCardsDealer = game.sumCardsDealer(dealersHand);
+        }
 
-    if (sumDealer <= 11) {
-        let dealer2 = `<div class="card" data-card-name="${shuffleCards.card3Dealer[0].name}">
-        <img class="show" src="${shuffleCards.card3Dealer[0].img}">
-        </div>
-        </div>`
-        document.querySelector('#dealer2').innerHTML = dealer2;
+        if (sumCardsDealer > 21) {
+            document.querySelector('#dealerSum').innerHTML = `<div class="points"> ${sumCardsDealer}</div>`
 
-        let card3Dealer = shuffleCards.card3Dealer[0].value;
+            let card = shuffleCards.cardsDealer.length -1
 
-        dealersHand.push(card3Dealer)
-        
-        sumDealer += card3Dealer
-    }
-  
-    let addDealersPoints = `<div class="points"> ${sumDealer}</div>`
-    document.querySelector('#dealerSum').innerHTML = addDealersPoints;
+            let dealer = `<img id="hideCard" class="show" src="${shuffleCards.cardsDealer[card].img}">`
 
-        
-        let compare = game.compareCards(sumPlayer,sumDealer)
-
-        if (!compare) {
-            let dealer = `<div class="card" data-card-name="${shuffleCards.cardsDealer[0].name}">
-            <img id="hideCard" class="show" src="${shuffleCards.cardsDealer[0].img}">
-                
-            </div>
-            <div class="card" data-card-name="${shuffleCards.cardsDealer[1].name}">
-            <img class="show" src="${shuffleCards.cardsDealer[1].img}">
-            </div>
-            </div>`;
-    
-            document.querySelector('#dealer').innerHTML = dealer;
-    
-            let lose = '<img src="./images/loser.png">'
-    
-            setTimeout(() => {
-    
-                document.querySelector('section').innerHTML = lose;
-            },1000)
-
+            game.winner();
             game.endGame();
 
         } else {
-            let dealer = `<div class="card" data-card-name="${shuffleCards.cardsDealer[0].name}">
-            <img id="hideCard" class="show" src="${shuffleCards.cardsDealer[0].img}">
-                
-            </div>
-            <div class="card" data-card-name="${shuffleCards.cardsDealer[1].name}">
-            <img class="show" src="${shuffleCards.cardsDealer[1].img}">
-            </div>
-            </div>`;
+            document.querySelector('#dealerSum').innerHTML = `<div class="points"> ${sumCardsDealer}</div>`
     
-            document.querySelector('#dealer').innerHTML = dealer;
-
-            let win = '<img src="./images/winner.png">'
+            let card = shuffleCards.cardsDealer.length -1
     
-            setTimeout(() => {
+            let dealer = `<img id="hideCard" class="show" src="${shuffleCards.cardsDealer[card].img}">`
     
-                document.querySelector('section').innerHTML = win;
-            },1000)
-
-            game.endGame();
-        }
-        
-    });
-
-    document.querySelector('.hit').addEventListener('click', () => {
-        let hitDisable = `<div class="buttonDisable">Hit</div>`;
-        document.querySelector('#hit').innerHTML = hitDisable;
-
-        let standDisable = `<div class="buttonDisable">Stand</div>`;
-        document.querySelector('#stand').innerHTML = standDisable;
-        
-        let player2 = `<div class="card" data-card-name="${shuffleCards.card3Player[0].name}">
-        <img class="show" src="${shuffleCards.card3Player[0].img}">
-        </div>
-        </div>`
-    document.querySelector('#player2').innerHTML = player2;
-
-    let card3Player = shuffleCards.card3Player[0].value
-
-    playersHand.push(card3Player)
-
-    let sumPlayer = game.sumCards(playersHand)
-        
-    let addPlayersPoints = `<div class="points"> ${sumPlayer}</div>`
-    document.querySelector('#playerSum').innerHTML = addPlayersPoints;
-
-    let sumDealer = game.sumCards(dealersHand)
-
-    if (sumDealer <= 11) {
-        let dealer2 = `<div class="card" data-card-name="${shuffleCards.card3Dealer[0].name}">
-        <img class="show" src="${shuffleCards.card3Dealer[0].img}">
-        </div>
-        </div>`
-        document.querySelector('#dealer2').innerHTML = dealer2;
-
-        let card3Dealer = shuffleCards.card3Dealer[0].value;
-
-        dealersHand.push(card3Dealer)
-        
-        sumDealer += card3Dealer
-    }
-    
-    let addDealersPoints = `<div class="points"> ${sumDealer}</div>`
-    document.querySelector('#dealerSum').innerHTML = addDealersPoints;
-
-    let compare = game.compareCards(sumPlayer,sumDealer)
-
-
-    if (!compare) {
-        let dealer = `<div class="card" data-card-name="${shuffleCards.cardsDealer[0].name}">
-        <img id="hideCard" class="show" src="${shuffleCards.cardsDealer[0].img}">
+                       
+            document.querySelector('#dealerHideCard').innerHTML = dealer;  
             
-        </div>
-        <div class="card" data-card-name="${shuffleCards.cardsDealer[1].name}">
-        <img class="show" src="${shuffleCards.cardsDealer[1].img}">
-        </div>
-        </div>`;
-
-        document.querySelector('#dealer').innerHTML = dealer;
-
-        let lose = '<img src="./images/loser.png">'
-
-        setTimeout(() => {
-
-            document.querySelector('section').innerHTML = lose;
-        },1000)
-        game.endGame();
-
-    } else {
-        let dealer = `<div class="card" data-card-name="${shuffleCards.cardsDealer[0].name}">
-            <img id="hideCard" class="show" src="${shuffleCards.cardsDealer[0].img}">
-                
-            </div>
-            <div class="card" data-card-name="${shuffleCards.cardsDealer[1].name}">
-            <img class="show" src="${shuffleCards.cardsDealer[1].img}">
-            </div>
-            </div>`;
+            console.log("dealer", shuffleCards.cardsDealer.length)
+            console.log("player", shuffleCards.cardsPlayer.length)
     
-            document.querySelector('#dealer').innerHTML = dealer;
-        let win = '<img src="./images/winner.png">'
-
-        setTimeout(() => {
-
-            document.querySelector('section').innerHTML = win;
-        },1000)
-        
-        game.endGame();
-    }
-    
-      
-    })    
-    
-})
+            if (sumCardsPlayer > sumCardsDealer) {
+                game.winner();
+                game.endGame();
+            } else if (sumCardsDealer > sumCardsPlayer) {
+                game.loser();
+                game.endGame();
+            } else {
+                if (shuffleCards.cardsDealer.length < shuffleCards.cardsPlayer.length) {
+                    game.loser();
+                    game.endGame();
+                } else {
+                    game.winner();
+                    game.endGame();
+                }
+            }
+        }    
+    });
+});
 
 
 
